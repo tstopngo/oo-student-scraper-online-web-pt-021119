85238b033d16a8f1d91d6a5523ea3a3d2ebefcb2
Tracy Ngo <tngo1206@gmail.com>
2019-04-22 19:44:32 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,6 +2,7 @@ require_relative "../lib/scraper.rb"
 require_relative "../lib/student.rb"
 require 'nokogiri'
 require 'colorize'
+require 'pry'
 
 class CommandLineInterface
   BASE_PATH = "./fixtures/student-site/"
